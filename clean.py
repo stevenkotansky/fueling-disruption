61d75cabb012911bd7174df16295888c02dbfc7e
Steven Kotansky <Steven.Kotansky@libertymutual.com>
2022-04-05 09:33:26 -0700

    Joined in datasets

--- clean.py
+++ clean.py
@@ -12,6 +12,14 @@ def in_lockdown(Date):
 
     return lockdown
 
+def convert_num_vehicles(Date):
+    if pd.to_datetime("3/26/2020") <= Date <= pd.to_datetime("5/29/2020"):
+        lockdown = 1
+    else:
+        lockdown = 0
+
+    return lockdown
+
 
 def find_season(month):
     # Winter (1)
@@ -124,36 +132,6 @@ df = df.rename(columns={"GasPrice": "Gas_Price", "bus": "Bus_Rides",
 df = df.drop(
     columns=["day_type"])
 
-# Connect to and format COVID case data
-df_covid = pd.read_csv("ChicagoCOVID.csv")
-df_covid = df_covid.rename(columns={
-    "Week Start": "Date", "Cases - Weekly": "Cases"})
-df_covid = df_covid[df_covid["Cases"] > 0]
-# need to fix days to join covid
-# df_covid["Date"] = df_covid["Date"].apply(lambda x: fix_date(x))
-df_covid["Date"] = pd.to_datetime(df_covid["Date"])
-df_covid = df_covid.drop(columns=["ZIP Code", "Week Number", "Week End", "Cases - Cumulative", "Case Rate - Weekly", "Case Rate - Cumulative", "Tests - Weekly", "Tests - Cumulative", "Test Rate - Weekly", "Test Rate - Cumulative",
-                         "Percent Tested Positive - Weekly", "Percent Tested Positive - Cumulative", "Deaths - Weekly", "Deaths - Cumulative", "Death Rate - Weekly", "Death Rate - Cumulative", "Population", "Row ID", "ZIP Code Location"])
-
-# Join COVID case data into dataset
-df = df.join(df_covid.set_index('Date'), on='Date')
-
-
-# Connect to IPUMS vehicle ownership dataset
-df_vehicle = pd.read_csv("ipums.csv")
-df_vehicle = df_vehicle[df_vehicle["VEHICLES"] > 0]
-# Add steps here to fix the blank, 0-9 coding
-df_vehicle = df_vehicle[["YEAR", "VEHICLES"]].groupby("YEAR").mean()
-x = df_vehicle.index.to_list()
-df_vehicle["Year"] = x
-
-df_vehicle = df_vehicle.rename(
-    columns={"YEAR": "Year", "VEHICLES": "Vehicle_Ownership"})
-print(df_vehicle)
-
-# Join vehicle ownership data into dataset
-df = df.join(df_vehicle, lsuffix='Year', rsuffix='Year')
-
 # Generate additional variables
 # Outcome variable 'ratio'
 df["Ratio"] = df["Transit_Ridership"]/df["TNP_Trips"]
@@ -165,7 +143,37 @@ df["Time"] = df["Date"].apply(
     lambda x: ((abs(pd.to_datetime("2018-11-05")-x).days)/7))
 df["Season"] = df["Month"].apply(lambda x: find_season(x))
 df["Lockdown"] = df["Date"].apply(lambda x: in_lockdown(x))
-# print(sum(df["Cases"]))
+
+# Connect to and format COVID case data
+df_covid = pd.read_csv("ChicagoCOVID.csv")
+df_covid = df_covid.rename(columns={
+    "Week Start": "Date", "Cases - Weekly": "Cases"})
+df_covid = df_covid[df_covid["Cases"] > 0]
+df_covid["Date"] = pd.to_datetime(df_covid["Date"])
+df_covid = df_covid.drop(columns=["ZIP Code", "Week Number", "Week End", "Cases - Cumulative", "Case Rate - Weekly", "Case Rate - Cumulative", "Tests - Weekly", "Tests - Cumulative", "Test Rate - Weekly", "Test Rate - Cumulative",
+                         "Percent Tested Positive - Weekly", "Percent Tested Positive - Cumulative", "Deaths - Weekly", "Deaths - Cumulative", "Death Rate - Weekly", "Death Rate - Cumulative", "Population", "Row ID", "ZIP Code Location"])
+
+# Aggregate COVID cases to be daily
+df_covid = df_covid.groupby(['Date'], as_index=False)['Cases'].sum()
+
+#Shift days to allign with other datasets
+df_cases = df_covid.set_index("Date")
+df_cases = df_cases.shift(freq="1D")
+# Join COVID case data into dataset
+df = df.join(df_cases, how="left", on='Date')
+
+# Input data from ACS car ownership data
+# 2021 data chosen to be the same as 2020 per this source showing it's pretty level https://www.valuepenguin.com/auto-insurance/car-ownership-statistics#state-per-capita
+df_veh_own = pd.DataFrame({"Year": [2018, 2019, 2020, 2021], "No_Vehicle_Pct": [26.9, 27.8, 26.8, 26.8]})
+# Join vehicle ownership data into dataset
+df = df.merge(df_veh_own, how = 'left', on='Year')
+
+# Input FRED Chicago population data
+df_population = pd.DataFrame({"Year": [2018, 2019, 2020, 2021], "Population": [9513947, 9485403, 9454282, 9601605]})
+# Join vehicle ownership data into dataset
+df = df.merge(df_population, how = 'left', on='Year')
+
+
 print(df)
 # Save the dataset to CSV
 try:
--- practice.py
+++ practice.py
@@ -1,7 +1,18 @@
-import datetime
-import numpy as np
 import pandas as pd
 
-print(pd.to_datetime("11/10/2018") > pd.to_datetime("11/11/2018"))
-print(pd.to_datetime("11/10/2018") < pd.to_datetime("11/11/2018"))
-print(pd.to_datetime("11/10/2018") == pd.to_datetime("11/10/2018"))
+# Connect to and format COVID case data
+df_covid = pd.read_csv("ChicagoCOVID.csv")
+df_covid = df_covid.rename(columns={
+    "Week Start": "Date", "Cases - Weekly": "Cases"})
+df_covid = df_covid[df_covid["Cases"] > 0]
+df_covid["Date"] = pd.to_datetime(df_covid["Date"])
+df_covid = df_covid.drop(columns=["ZIP Code", "Week Number", "Week End", "Cases - Cumulative", "Case Rate - Weekly", "Case Rate - Cumulative", "Tests - Weekly", "Tests - Cumulative", "Test Rate - Weekly", "Test Rate - Cumulative",
+                         "Percent Tested Positive - Weekly", "Percent Tested Positive - Cumulative", "Deaths - Weekly", "Deaths - Cumulative", "Death Rate - Weekly", "Death Rate - Cumulative", "Population", "Row ID", "ZIP Code Location"])
+
+# Aggregate COVID cases to be daily
+df_covid = df_covid.groupby(['Date'], as_index=False)['Cases'].sum()
+
+#Shift days to allign with other datasets
+df_cases = df_covid.set_index("Date")
+df_cases = df_cases.shift(freq="1D")
+print(df_cases)
